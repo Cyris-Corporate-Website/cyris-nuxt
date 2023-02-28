7fa32917deba0d57130a567ebfff19dfb8328a7b
Martin Stratiev <martin.s@voidweb.eu>
2023-02-28 14:32:29 +0200

    configured tailwind

---
+++ pages/about/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-blue-500">
+    about
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-red-500">home <NuxtLink to="/about">About</NuxtLink></div>
+</template>
