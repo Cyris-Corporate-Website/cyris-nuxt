a158c342231e66173203ace8d5a38b202f45faec
Martin Stratiev <martin.s@voidweb.eu>
2023-03-10 20:19:06 +0200

    fixed burger menu

--- components/base/BaseDropdown.vue
+++ components/base/BaseDropdown.vue
@@ -1,37 +1,22 @@
 <template>
-  <div class="w-full px-4 pt-16">
-    <div class="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+  <div class="w-full px-4">
+    <div class="w-full rounded-2xl bg-white p-2 font-body">
       <Disclosure v-slot="{ open }">
         <DisclosureButton
-          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+          class="flex w-full justify-between py-2 text-left text-sm"
         >
-          <span>What is your refund policy?</span>
+          <common-nav-link class="font-semibold">
+            <slot name="header" />
+          </common-nav-link>
           <img
             src="@/assets/images/home/icons/chevron-black.svg"
-            :class="open ? 'rotate-180 transform' : ''"
+            :class="open ? 'rotate-90 transform' : ''"
             class="h-5 w-5 text-purple-500"
             alt=""
           />
         </DisclosureButton>
-        <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-          If you're unhappy with your purchase for any reason, email us within
-          90 days and we'll refund you in full, no questions asked.
-        </DisclosurePanel>
-      </Disclosure>
-      <Disclosure as="div" class="mt-2" v-slot="{ open }">
-        <DisclosureButton
-          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-        >
-          <span>Do you offer technical support?</span>
-          <img
-            src="@/assets/images/home/icons/chevron-black.svg"
-            :class="open ? 'rotate-180 transform' : ''"
-            class="h-5 w-5 text-purple-500"
-            alt=""
-          />
-        </DisclosureButton>
-        <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-          No.
+        <DisclosurePanel class="pt-4 pb-2 text-sm text-gray-600">
+          <slot name="items" />
         </DisclosurePanel>
       </Disclosure>
     </div>
--- components/common/CommonMobileHeader.vue
+++ components/common/CommonMobileHeader.vue
@@ -8,10 +8,10 @@
     </div>
     <div
       v-show="isVisible"
-      class="absolute top-0 left-0 bg-white text-gray-600 rounded-b-[15px] min-h-[515px] min-w-full z-50"
+      class="absolute flex flex-col justify-between pb-10 top-0 left-0 bg-white text-gray-600 rounded-b-[15px] min-h-[515px] min-w-full z-50"
     >
-      <div class="flex flex-col justify-between">
-        <div class="px-4 flex justify-between items-center w-full">
+      <div>
+        <div class="px-4 py-5 flex justify-between items-center w-full">
           <common-logo :dark="true" class="mr-5" />
           <img
             @click="isVisible = !isVisible"
@@ -20,33 +20,101 @@
             alt=""
           />
         </div>
-        <base-dropdown> </base-dropdown>
+        <base-dropdown>
+          <template #header>Services</template>
+          <template #items>
+            <div class="space-y-3.5">
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/Group-172636.svg"
+                  alt=""
+                />
+                <p>Card Scheme Connectivity</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/Acquirers’-Processing.svg"
+                  alt=""
+                />
+                <p>Acquirers’ Processing</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/Issuers’-Processing.svg"
+                  alt=""
+                />
+                <p>Issuers’ Processing</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/Clearing-and-Settlement.svg"
+                  alt=""
+                />
+                <p>Clearing and Settlement</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/Security-and-compliance.svg"
+                  alt=""
+                />
+                <p>Security and compliance</p>
+              </div>
+            </div>
+          </template>
+        </base-dropdown>
+        <base-dropdown>
+          <template #header>Platform</template>
+          <template #items>
+            <div class="space-y-3.5">
+              <div class="flex items-end space-x-5">
+                <img src="@/assets/images/home/mobileHeader/API.svg" alt="" />
+                <p>API</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img src="@/assets/images/home/mobileHeader/CMS.svg" alt="" />
+                <p>CMS</p>
+              </div>
+            </div>
+          </template>
+        </base-dropdown>
+        <base-dropdown>
+          <template #header>Company</template>
+          <template #items>
+            <div class="space-y-3.5">
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/About-us.svg"
+                  alt=""
+                />
+                <p>About us</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img
+                  src="@/assets/images/home/mobileHeader/Careers.svg"
+                  alt=""
+                />
+                <p>Careers</p>
+              </div>
+              <div class="flex items-end space-x-5">
+                <img src="@/assets/images/home/mobileHeader/News.svg" alt="" />
+                <p>News</p>
+              </div>
+            </div>
+          </template>
+        </base-dropdown>
+        <common-nav-link
+          class="px-6 pr-7 py-4 font-semibold flex justify-between items-center"
+        >
+          <p>Developers</p>
+          <img src="@/assets/images/home/icons/chevron-black.svg" alt=">" />
+        </common-nav-link>
+      </div>
+
+      <div class="flex justify-center text-center">
         <div>
-          <common-nav-link class="px-6 flex justify-between items-center">
-            <h2>Services</h2>
-            <img src="@/assets/images/home/icons/chevron-black.svg" alt=""
-          /></common-nav-link>
-          <common-nav-link class="px-6 flex justify-between items-center">
-            <h2>Platform</h2>
-            <img
-              src="@/assets/images/home/icons/chevron-black.svg"
-              alt="" /></common-nav-link
-          ><common-nav-link class="px-6 flex justify-between items-center">
-            <h2>Company</h2>
-            <img
-              src="@/assets/images/home/icons/chevron-black.svg"
-              alt="" /></common-nav-link
-          ><common-nav-link class="px-6 flex justify-between items-center">
-            <h2>Developers</h2>
-            <img src="@/assets/images/home/icons/chevron-black.svg" alt=""
-          /></common-nav-link>
-        </div>
-        <div class="flex justify-center text-center">
-          <div>
-            <hr class="w-96" />
-            <base-button class="mb-3.5 mt-4">Contact us</base-button>
-            <h2>Login</h2>
-          </div>
+          <hr class="w-[330px]" />
+          <base-button class="mb-3.5 mt-4">Contact us</base-button>
+          <h2>Login</h2>
         </div>
       </div>
     </div>
