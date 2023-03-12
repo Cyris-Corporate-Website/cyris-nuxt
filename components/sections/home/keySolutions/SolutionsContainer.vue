a0d3a5a341083a9641dd7c6024c944ebdd13b127
Martin Stratiev <martin.s@voidweb.eu>
2023-03-12 17:36:03 +0200

    changes

--- components/sections/home/keySolutions/SolutionsContainer.vue
+++ components/sections/home/keySolutions/SolutionsContainer.vue
@@ -2,75 +2,19 @@
   <div class="sm:flex justify-between items-start">
     <div class="space-y-20">
       <sub-heading color="black">Explore Our Solutions</sub-heading>
-      <solution-heading @mouseover="switchCard(0)"
-        >Card Scheme Connectivity</solution-heading
-      >
-      <solution-heading @mouseover="switchCard(1)"
-        >Acquirers’ Processing</solution-heading
-      >
-      <solution-heading @mouseover="switchCard(2)"
-        >Issuers’ Processing</solution-heading
-      >
-      <solution-heading @mouseover="switchCard(3)"
-        >Clearing and Sattlement</solution-heading
-      >
-      <solution-heading @mouseover="switchCard(4)"
-        >Security and compliance</solution-heading
-      >
+      <div v-for="card in solutionCards" :key="card.id">
+        <solution-heading @mouseover="switchCard(card.id)">{{
+          card.heading
+        }}</solution-heading>
+      </div>
     </div>
-    <SolutionsCard v-show="cardNumber == 0">
-      <template #img
-        ><img src="@/assets/images/home/card-connectivity.png" alt=""
-      /></template>
-      <template #text
-        >Extend your payments reach and streamline costs, connect with multiple
-        national and international networks, channels and acquirers and
-        authorize operations in real time.</template
-      >
-    </SolutionsCard>
-
-    <SolutionsCard v-show="cardNumber == 1">
-      <template #img
-        ><img src="@/assets/images/home/POS.png" alt=""
-      /></template>
-      <template #text
-        >Join the next generation of acquirers. Our flexible, modern technology
-        enables acquirers to deliver innovative omnichannel journeys.</template
-      >
-    </SolutionsCard>
-
-    <SolutionsCard v-show="cardNumber == 2">
-      <template #img
-        ><img src="@/assets/images/home/Issuers’-Processing.png" alt=""
-      /></template>
-      <template #text
-        >Turnkey card-issuing and program management, simplified for growing
-        Fintech companies, traditional bank players and e-money
-        institutions.</template
-      >
-    </SolutionsCard>
-
-    <SolutionsCard v-show="cardNumber == 3">
-      <template #img
-        ><img src="@/assets/images/home/Clearing-and-Sattlement.png" alt=""
-      /></template>
-      <template #text
-        >Preparation of periodic reports regarding international and local card
-        schemes on behalf of the institution.</template
-      >
-    </SolutionsCard>
-
-    <SolutionsCard v-show="cardNumber == 4">
-      <template #img
-        ><img src="@/assets/images/home/key-lock.png" alt=""
-      /></template>
-      <template #text
-        >Sitting behind every transaction which customers make, Cyris conducts a
-        series of instantaneous checks and verifications to ensure secure, fast,
-        accurate, resilient, and PCI-compliant authorization and settlement
-        processing.</template
-      >
-    </SolutionsCard>
+    <div v-for="card in solutionCards" :key="card.id">
+      <SolutionsCard
+        v-show="cardNumber == card.id"
+        :description="card.description"
+        :img-name="card.imgName"
+      />
+    </div>
   </div>
 </template>
 <script setup>
@@ -82,4 +26,42 @@ const cardNumber = ref(0)
 const switchCard = (cn) => {
   cardNumber.value = cn
 }
+
+const solutionCards = [
+  {
+    id: 0,
+    heading: 'Card Scheme Connectivity',
+    description:
+      'Extend your payments reach and streamline costs, connect with multiple national and international networks, channels and acquirers and authorize operations in real time.',
+    imgName: 'card-connectivity.png',
+  },
+  {
+    id: 1,
+    heading: 'Acquirers’ Processing',
+    description:
+      'Join the next generation of acquirers. Our flexible, modern technology enables acquirers to deliver innovative omnichannel journeys.',
+    imgName: 'POS.png',
+  },
+  {
+    id: 2,
+    heading: 'Issuers’ Processing',
+    description:
+      'Turnkey card-issuing and program management, simplified for growing Fintech companies, traditional bank players and e-money institutions.',
+    imgName: 'Issuers’-Processing.png',
+  },
+  {
+    id: 3,
+    heading: 'Clearing and Sattlement',
+    description:
+      'Preparation of periodic reports regarding international and local card schemes on behalf of the institution.',
+    imgName: 'Clearing-and-Sattlement.png',
+  },
+  {
+    id: 4,
+    heading: 'Security and compliance',
+    description:
+      'Sitting behind every transaction which customers make, Cyris conducts a series of instantaneous checks and verifications to ensure secure, fast, accurate, resilient, and PCI-compliant authorization and settlement processing.',
+    imgName: 'key-lock.png',
+  },
+]
 </script>
--- components/sections/home/keySolutions/SolutionsContainerMobile.vue
+++ components/sections/home/keySolutions/SolutionsContainerMobile.vue
@@ -20,30 +20,35 @@ import SolutionsCard from './SolutionsCard.vue'
 
 const solutionCards = [
   {
+    id: 0,
     heading: 'Card Scheme Connectivity',
     description:
       'Extend your payments reach and streamline costs, connect with multiple national and international networks, channels and acquirers and authorize operations in real time.',
     imgName: 'card-connectivity.png',
   },
   {
+    id: 1,
     heading: 'Acquirers’ Processing',
     description:
       'Join the next generation of acquirers. Our flexible, modern technology enables acquirers to deliver innovative omnichannel journeys.',
     imgName: 'POS.png',
   },
   {
+    id: 2,
     heading: 'Issuers’ Processing',
     description:
       'Turnkey card-issuing and program management, simplified for growing Fintech companies, traditional bank players and e-money institutions.',
     imgName: 'Issuers’-Processing.png',
   },
   {
+    id: 3,
     heading: 'Clearing and Sattlement',
     description:
       'Preparation of periodic reports regarding international and local card schemes on behalf of the institution.',
     imgName: 'Clearing-and-Sattlement.png',
   },
   {
+    id: 4,
     heading: 'Security and compliance',
     description:
       'Sitting behind every transaction which customers make, Cyris conducts a series of instantaneous checks and verifications to ensure secure, fast, accurate, resilient, and PCI-compliant authorization and settlement processing.',
